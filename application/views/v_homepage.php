6c6b1640ddeca38772136950520bf81c1767045c
archiecakra <archiecakra1@gmail.com>
2018-09-08 05:58:46 +0700

    tambah maps

--- application/views/v_homepage.php
+++ application/views/v_homepage.php
@@ -47,19 +47,18 @@
           </div>
         </div>
       </nav>
-      <div class="container">
+      <div class="container-fluid">
         <div class="row">
           <div class="banner-info">
-            <div class="banner-text text-center">
-              <h1 class="white">Welcome</h1>
-              <p>Temukan Faskes Terdekat!!</p>
+            <!-- start maps -->
+            <div class="iframe-container">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31657.504880396624!2d112.74402188814592!3d-7.332759418138233!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x447eceaea21cd258!2sRS+Royal+Surabaya!5e0!3m2!1sid!2sid!4v1536358196134" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
             </div>
-<!-- start maps -->
+            <!-- end maps -->
 
-<!-- end maps -->
-            <div class="overlay-detail text-center">
-              <a href="#service"><i class="fa fa-angle-down"></i></a>
-            </div>
+          </div>
+          <div class="overlay-detail text-center">
+            <a href="#service"><i class="fa fa-angle-down" style="bottom: 100px;"></i></a>
           </div>
         </div>
       </div>
