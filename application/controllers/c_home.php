25725307b206843df29db3ba4cadd396576c04d0
archiecakra <archiecakra1@gmail.com>
2018-09-11 06:13:04 +0700

    coba fix 404 not found di server

--- application/controllers/c_home.php
+++ application/controllers/c_home.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class c_home extends CI_Controller {
+class C_home extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
