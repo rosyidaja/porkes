5f60cae68a88e5eab482da93c1fc9dc6fe202184
Rosyid Mochammad <rosyidaja21@gmail.com>
2018-09-11 13:29:38 +0700

    merubah navbar pada page

--- application/views/v_maps.php
+++ application/views/v_maps.php
@@ -1,14 +1,9 @@
       <div class="container-fluid">
         <div class="row">
-          <div class="banner-info" style="margin-top: 110px;">
+          <div class="banner-info" style="margin-top: 90px;">
             <!-- start maps -->
-            <div class="iframe-container">
-<<<<<<< HEAD
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31657.504880396624!2d112.74402188814592!3d-7.332759418138233!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x447eceaea21cd258!2sRS+Royal+Surabaya!5e0!3m2!1sid!2sid!4v1536358196134" width="600" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
-=======
-              <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31657.504880396624!2d112.74402188814592!3d-7.332759418138233!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x447eceaea21cd258!2sRS+Royal+Surabaya!5e0!3m2!1sid!2sid!4v1536358196134" width="600" height="500" frameborder="0" style="border:0" allowfullscreen></iframe> -->
+            <div class="iframe-container">              
               <iframe src="https://www.google.com/maps/embed/v1/search?q=hospital%20near%20my%20location&key=AIzaSyApbqMy_ftrIpADq5OxKnqPXrFydE76-jA" width="600" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
->>>>>>> 07727719f6e47f8fbd1ddf6157af7060fc723d57
             </div>
             <!-- end maps -->
 
--- application/views/v_page.php
+++ application/views/v_page.php
@@ -10,7 +10,29 @@
   <section id="banner" class="banner">
     <div class="bg-color">
       <!-- navbar -->
-        <?php $this->load->view($navbar); ?>
+      <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+          <div class="col-md-12">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#"><img src="<?php echo base_url('assets/img/logo.png');?>" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>
+            </div>
+            <div class="collapse navbar-collapse navbar-right" id="myNavbar">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="<?php echo base_url(); ?>">Home</a></li>
+                <li class=""><a href="#">Artikel</a></li>
+                <li class=""><a href="#">Layanan</a></li>
+                <li class=""><a href="#">Faskes</a></li>
+                <!-- <li class=""><a href="#contact">Contact</a></li> -->
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
         <!-- /navbar -->
           <!--service-->
   <?php $this->load->view($list_artikel); ?>
