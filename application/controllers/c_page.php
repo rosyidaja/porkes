b5264e8c6fd2dddd21a52debb1450d455dbcaa05
tomi <tomimishbahul@gmail.com>
2018-09-11 13:34:39 +0700

    not found

--- application/controllers/c_page.php
+++ application/controllers/c_page.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class c_page extends CI_Controller {
+class C_page extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
