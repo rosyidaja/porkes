bb64d5633c4c2c9ce592eb2f56f5d41a0f44c9b2
archiecakra <archiecakra1@gmail.com>
2018-09-12 16:49:20 +0700

    tambah c_detail_faskes & v_detail_faskes

---
+++ application/controllers/c_detail_faskes.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class C_detail_faskes extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$data['head_top_resource'] = 'v_head_top_resource';
+		$data['maps'] = 'v_maps';
+		$data['navbar'] = 'v_navbar';
+		$data['content'] = 'v_detail_faskes';
+		$data['footer'] = 'v_footer';
+		$data['bottom_resource'] = 'v_bottom_resource';
+        $this->load->view('v_page',$data);
+	}
+}
---
+++ application/views/v_detail_faskes.php
@@ -0,0 +1,87 @@
+<section id="doctor-team" class="secton-padding"  style="background-color:#eff3f8;">
+  <div class="container">
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="jumbotron" style="margin-bottom:0; border-radius:0;">
+          <h1>Gambar Preview Rumah Sakit</h1>
+          <p>Gambar prevew</p>
+        </div>
+        <div class="panel" style="border-radius:0; border:0;">
+          <div class="panel-body">
+            <p>Keterangan Rumah Sakit</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-3">
+
+        <div class="panel">
+          <div class="panel-body">
+
+            <div class="page-header">
+              <label>LOKASI</label>
+            </div>
+
+            <div style="width: 100%"><iframe width="100%" height="358" src="https://maps.google.com/maps?width=100%&amp;height=358&amp;hl=en&amp;q=surabaya+(Porkes)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/create-google-map/">Embed Google Map</a></iframe></div><br />
+
+          </div>
+        </div>
+
+        <div class="panel">
+          <div class="panel-body">
+
+            <div class="page-header">
+              <label>LAYANAN</label>
+            </div>
+
+            <div class="small">
+              <p><i class="fa fa-check"></i> Ambulance</p>
+              <p><i class="fa fa-check"></i> Kamar Enak Enak</p>
+              <p><i class="fa fa-check"></i> Instalasi Gawat Darurat (IGD)</p>
+              <p><i class="fa fa-check"></i> Klinik Bedah Kepala & Leher</p>
+              <p><i class="fa fa-check"></i> Klinik Gigi & Kesehatan Cocot</p>
+              <p><i class="fa fa-check"></i> Klinik Gigi & Kesehatan Cocot</p>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col-md-9">
+        <div class="panel">
+          <div class="panel-body">
+
+            <ul class="nav nav-tabs">
+              <li class="active"><a data-toggle="tab" href="#home">Poli</a></li>
+              <li><a data-toggle="tab" href="#menu1">Dokter</a></li>
+              <li><a data-toggle="tab" href="#menu2">Galeri</a></li>
+              <li><a data-toggle="tab" href="#menu2">Profil</a></li>
+              <li><a data-toggle="tab" href="#menu2">Rating</a></li>
+            </ul>
+
+            <div class="tab-content">
+              <div id="home" class="tab-pane fade in active">
+                <h3>HOME</h3>
+                <p>Some content.</p>
+              </div>
+              <div id="menu1" class="tab-pane fade">
+                <h3>Menu 1</h3>
+                <p>Some content in menu 1.</p>
+              </div>
+              <div id="menu2" class="tab-pane fade">
+                <h3>Menu 2</h3>
+                <p>Some content in menu 2.</p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
