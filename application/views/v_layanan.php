be0f9e77116f744024b255e31429ba4ed95363e7
tomi <tomimishbahul@gmail.com>
2018-09-13 14:36:48 +0700

    v_layanan.php

--- application/views/v_layanan.php
+++ application/views/v_layanan.php
@@ -1,51 +1,66 @@
 <section id="service" class="section-padding">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4 col-sm-4">
-          <h2 class="ser-title">Our Service</h2>
-          <hr class="botm-line">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris cillum.</p>
-        </div>
-        <div class="col-md-4 col-sm-4">
-          <div class="service-info">
-            <div class="icon">
-              <i class="fa fa-stethoscope"></i>
-            </div>
-            <div class="icon-info">
-              <h4>24 Hour Support</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-          </div>
-          <div class="service-info">
-            <div class="icon">
-              <i class="fa fa-ambulance"></i>
-            </div>
-            <div class="icon-info">
-              <h4>Emergency Services</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-sm-4">
+        <h2 class="ser-title">Layanan</h2>
+        <hr class="botm-line">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4 col-sm-4">
+        <div class="service-info" align="center">
+            <img width="100px;" class="img-responsive" src="<?php echo base_url('assets/icons/umum.png');?>">
+          <div class="icon-info">
+            <h4>Dokter</h4>
+            <p>Layanan Direktori dokter berisi informasi dokter yang meliputi lokasi, biografi, jadwal praktek, dan biaya pelayanan.</p>
           </div>
         </div>
-        <div class="col-md-4 col-sm-4">
-          <div class="service-info">
-            <div class="icon">
-              <i class="fa fa-user-md"></i>
-            </div>
-            <div class="icon-info">
-              <h4>Medical Counseling</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
+      </div>
+      <div class="col-md-4 col-sm-4">
+        <div class="service-info" align="center">
+            <img width="100px;" class="img-responsive" src="https://medismap.com/theme/global/images/service/b.png">
+          <div class="icon-info">
+            <h4>Fasilitas Kesehatan</h4>
+            <p>Layanan direktori fasilitas kesehatan yang berisi info dari pusat kesehatan mulai dari jumlah poli, jumlah dokter, asuransi, dsb.</p>
           </div>
-          <div class="service-info">
-            <div class="icon">
-              <i class="fa fa-medkit"></i>
-            </div>
-            <div class="icon-info">
-              <h4>Premium Healthcare</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-4">
+        <div class="service-info" align="center">
+            <img width="100px;" class="img-responsive" src="<?php echo base_url('assets/icons/umum.png');?>">
+          <div class="icon-info">
+            <h4>24 Hour Support</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-4">
+        <div class="service-info" align="center">
+            <img width="100px;" class="img-responsive" src="<?php echo base_url('assets/icons/umum.png');?>">
+          <div class="icon-info">
+            <h4>24 Hour Support</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-4">
+        <div class="service-info" align="center">
+            <img width="100px;" class="img-responsive" src="<?php echo base_url('assets/icons/umum.png');?>">
+          <div class="icon-info">
+            <h4>24 Hour Support</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-4">
+        <div class="service-info" align="center">
+            <img width="100px;" class="img-responsive" src="<?php echo base_url('assets/icons/umum.png');?>">
+          <div class="icon-info">
+            <h4>24 Hour Support</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
+</section>
