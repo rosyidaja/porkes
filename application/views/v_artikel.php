37928c930dc058ef84c7f16cc557286c8863dabb
Rosyid Mochammad <rosyidaja21@gmail.com>
2018-11-14 17:03:00 +0700

    substring paragraf artikel

--- application/views/v_artikel.php
+++ application/views/v_artikel.php
@@ -14,8 +14,8 @@
                 <img src="<?php echo base_url();?>assets/upload/artikel/<?php echo $value->artikel_foto;?>" class="team-img" style="height: 250px; width: 250px;">
                 <div class="caption">
                 <h4><?php echo $value->artikel_judul ?></h4>
-                <p><?php echo $value->artikel_isi ?></p>
-                <a href="<?php echo base_url('C_artikel/detail_artikel/'.$value->artikel_id)?>">Lihat Artikel ..</a>
+                <p><?php echo substr($value->artikel_isi,0,100)."..." ?></p>
+                <a href="<?php echo base_url('C_artikel/detail_artikel/'.$value->artikel_id)?>">Lihat Artikel</a>
                 </div>
               </div>
             </div>
@@ -24,7 +24,7 @@
         </div>
       </div>
       <div class="box-footer text-right">
-        <a href="<?php echo base_url().'C_artikel/list_artikel';?>" class="uppercase">Lihat Semua ..</a>
+        <a href="<?php echo base_url().'C_artikel/list_artikel';?>" class="uppercase">Lihat Semua</a>
       </div>
       <!-- <div class="pull-right">
         <a href="<?php echo base_url().'C_artikel/list_artikel';?>">Lihat Semua</a>
