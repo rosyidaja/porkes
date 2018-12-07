1fce2a28ff985c87bf55479667a58114ef3f1554
tomi <tomimishbahul@gmail.com>
2018-12-07 19:44:33 +0700

    navbar

--- application/views/v_navbar.php
+++ application/views/v_navbar.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href=""><img src="<?php echo base_url('assets/img/logo.png');?>" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>
+              <a class="navbar-brand" href="<?php echo base_url('C_home/index'); ?>"><img src="<?php echo base_url('assets/img/logo.png');?>" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>
             </div>
             <div class="collapse navbar-collapse navbar-right" id="myNavbar">
               <ul class="nav navbar-nav">
