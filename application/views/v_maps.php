17eb88e009b479a4dde57101fb9f2545c9848bde
tomi <tomimishbahul@gmail.com>
2018-12-08 08:19:08 +0700

    new

--- application/views/v_maps.php
+++ application/views/v_maps.php
@@ -1,6 +1,6 @@
       <div class="container-fluid">
         <div class="row">
-          <div class="banner-info" style="margin-top: 90px; margin-bottom: 50px;">
+          <div class="banner-info" style="margin-top: 90px;  margin-bottom: 50px;">
             <!-- start maps -->
             <!-- <div class="iframe-container">
               <iframe src="https://www.google.com/maps/embed/v1/search?q=hospital%20near%20my%20location&key=AIzaSyApbqMy_ftrIpADq5OxKnqPXrFydE76-jA" width="600" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
